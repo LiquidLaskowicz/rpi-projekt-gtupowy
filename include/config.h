c1e64d4aee6521c0445b18460fdf7d3e78603c5f
LiquidLaskowicz <mixerak123@gmail.com>
2026-01-13 15:18:53 +0100

    poprawa adresowania uartu rpi

--- include/config.h
+++ include/config.h
@@ -8,8 +8,8 @@
 #define APP_VERSION "0.1.0"
 
 // UART
-#define UART_DEVICE "/dev/serial0" // urzadzenie UART
-#define UART_BAUDRATE 9600         // predkosc transmisji
+#define UART_DEVICE "/dev/ttyAMA10" // urzadzenie UART
+#define UART_BAUDRATE 9600          // predkosc transmisji
 
 // Debugowanie [1 - DEBUG ON / 0 - DEBUG OFF]
 #define DEBUG 1
